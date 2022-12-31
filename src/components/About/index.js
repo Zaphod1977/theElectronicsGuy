891a3f285c8e8a9dd6fe7362aa6bd1237250e7a2
carfiguru@hotmail.com <carfiguru@hotmail.com>
2022-12-30 18:33:15 -0600

    added telephone launch off of button touch, styled about page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,7 +5,31 @@ function About() {
       <h2 id="about" className="aboutme-leftside">About</h2>
       <p className="aboutme-rightside">
 
-        &emsp;Hello, my name is Bobby Bridges. I am a father, grandfather, soccer fan and independant business owner. I have
+        <ol className='aboutfloat'> &emsp; July 2000 - Accepted a sales position with Hi-Fi Fo-Fum on Big Bend.</ol>
+        <br></br>
+        <ol className='aboutfloat'> &emsp; July 2003 - Accepted a position with MDB Electric to learn how to do install at a 40% pay decrease. At MDB 
+                    we wired homes from 1,000 sqft up to 10,000. There I learned proper wire infrustructure
+        </ol>
+        <br></br>
+        <ol className='aboutfloat'> &emsp; August 2004 - Accepted a position with Custom Entertainment Tech as a full time installer.  Servicing clients in the most
+                    exlusive locations with the best systems in the city. 2,000 sqft to 20,000 sqft in scale.
+        </ol>
+        <br></br>
+        <ol className='aboutfloat'> &emsp; September 2005 - Accepted a position with Music for Pleasure as a lead tech, design consultant and part time sales.
+                    With Music for Pleasure I built on my knowledge of high end system design concepts and system synergies.
+        </ol>
+        <br></br>
+        <ol className='aboutfloat'> &emsp; July 2010 - Established The Electronics Guy as an independant technology integrator. Earning clients from every
+                    walk of life. From fellow blue collar guys to CEO's of forture 500 companies (and their families).
+        </ol>
+        <br></br>
+        <br></br>
+        {/* <iframe src="https://open.spotify.com/embed/episode/2DG3ToOGFZ8wD8Dh29OCkK?utm_source=generator" 
+        width="50%" height="352"  allowfullscreen="" 
+        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe> */}
+        <br></br>
+        <br></br>
+        {/* &emsp;Hello, my name is Bobby Bridges. I am a father, grandfather, soccer fan and independant business owner. I have
         been in the tech world since I was a kid taking
         audio gear apart to figure out how it worked and how to make it do different things I wanted to make it do.
         That enginuity brought me to the audio/video and automation
@@ -18,7 +42,7 @@ function About() {
         My true skill is making technology nice and approachable for almost anyone.  Regardless of age or technical
         propensity.  Lets explore whats possible and make those tech dreams come true!
         <br></br>
-        <br></br>
+        <br></br> */}
 
       </p>
     </section>
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,11 +4,11 @@ function Footer() {
     <footer className='space'>
         <div id="con">
             {/* <a href="https://avatars.githubusercontent.com/u/103078477?v=4"></a> */}
-            <a>314-853-0016</a>
-            <a href="https://www.linkedin.com/in/bobby-bridges-baa54889/" target="_blank">Linkedin</a>
-            <a href="https://github.com/Zaphod1977/" target="_blank">Github</a>
-            <a href="https://www.instagram.com/bobby_bridges_teg/?hl=en" target="_blank">Instagram</a>
-            <a href="https://open.spotify.com/show/2ip0Ojn7AwzukeawerfPDE" target="_blank">Spotify</a>
+            <a href="tel:3148530016">314-853-0016</a>
+            {/* <a href="https://www.linkedin.com/in/bobby-bridges-baa54889/" target="_blank">Linkedin</a> */}
+            {/* <a href="https://github.com/Zaphod1977/" target="_blank">Github</a> */}
+            {/* <a href="https://www.instagram.com/bobby_bridges_teg/?hl=en" target="_blank">Instagram</a> */}
+            {/* <a href="https://open.spotify.com/show/2ip0Ojn7AwzukeawerfPDE" target="_blank">Spotify</a> */}
         </div>
     </footer>
   );
